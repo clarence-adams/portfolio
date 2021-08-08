f244afc6cf05cecf6565473bb6f7a74f58c167fc
clarence-de-coder <clarence.e.a@gmail.com>
2021-08-08 16:36:19 -0400

    Moved project array to a seperate .json file

--- src/App.js
+++ src/App.js
@@ -4,45 +4,13 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 import {faWrench} from '@fortawesome/free-solid-svg-icons'
+// array containing info for project tiles
+const projects = require('./projects.json')
 
 library.add(fab, faWrench)
 
 // app
 function App() {
-  // array of objects that hold info needed to construct tile components
-  const tiles = [
-    {
-      'id': 'small-business',
-      'title': 'small business',
-      'url': 'http://www.sloneheatingandair.com'
-    },
-    {
-      'id': 'calculator',
-      'title': 'calculator',
-      'url': 'https://flamboyant-kalam-8ca516.netlify.app'
-    },
-    {
-      'id': 'pomodoro-timer',
-      'title': 'pomodoro timer',
-      'url': 'https://suspicious-noether-2de41b.netlify.app'
-    },
-    {
-      'id': 'drum-machine',
-      'title': 'drum machine',
-      'url': 'https://happy-curie-b68712.netlify.app'
-    },
-    {
-      'id': 'random-quote-generator',
-      'title': 'random quote generator',
-      'url': 'https://clarence-de-coder.github.io/fcc-random-quote-machine'
-    },
-    {
-      'id': 'markdown-previewer',
-      'title': 'markdown previewer',
-      'url': 'https://quizzical-volhard-f6d8e1.netlify.app'
-    }
-  ]
-
   return (
     <div id='app' className='App'>
       <header>
@@ -68,7 +36,7 @@ function App() {
           <p><i>currently under development but fully functional <FontAwesomeIcon icon='wrench'/></i></p>
         </div>
         <div id='project-tiles'>
-          {tiles.map((element) =>
+          {projects.map((element) =>
             <Tile id={element.id} title={element.title} url={element.url}/>
           )}
         </div>
